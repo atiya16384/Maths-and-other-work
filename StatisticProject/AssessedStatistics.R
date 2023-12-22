e6bf0ec1a34bd3607bea730bcd1e5186083611af
Atiya Mahboob <atiyahussain17170@outlook.com>
2023-12-22 01:19:10 +0000

    fix: refactored the code by using functions etc.

--- StatisticProject/AssessedStatistics.R
+++ StatisticProject/AssessedStatistics.R
@@ -1,48 +1,59 @@
+calculate_mean <- function(data) {
+    mean(data)
+}
+
+calculate_sd <- function(data) {
+    sd(data)
+}
+
+
+calculate_se <- function(sd, n) {
+    sd / sqrt(n)
+}
+
+calculate_ci <- function(mean, se, confidence_level) {
+    z <- qnorm((1 + confidence_level) / 2)
+    lower_bound = mean - z * se
+    upper_bound = mean + z * se
+    c(lower_bound, upper_bound)
+}
+
+hypothesis_test <- function(sample_mean, mu, se) {
+    z_score = (sample_mean - mu) / se
+    p_value = 2 * pnorm(-abs(z_score))
+    list(z_score = z_score, p_value = p_value)
+}
+
+plot_histogram_qq <- function(data) {
+    histogram <- hist(data, prob = TRUE)
+    lines(density(data))
+    qqnorm(data)
+    qqline(data)
+    list(histogram = histogram)
+}
+
 load(file = "PROJ.Rdata")
 mydata = PROJ[257,]
+n = 40
 
-#number of data points
-n=40
-n
-#calculate the sample mean
-mx=mean(PROJ[257,])
-mx
-#calculate the sample standard deviation
-sampleSD=sd(PROJ[257,])
-sampleSD
-#plot histogram of data against normal distribution
-histogram=hist(PROJ[257,], prob= TRUE)
-lines(density(PROJ[257,]))
-#plot Q-Q plot of data against the normal distribution
-QQPlot=qqnorm(PROJ[257,]);abline(mx,sampleSD)
-QQPlot
-#Calculate the standard error of the mean
-se=(sampleSD)/sqrt(n)
-se
-#calculate a 95% confidence interval
-zstar1=qnorm(0.975)
-zstar1
-CI1=c(mx-zstar1*se, mx+zstar1*se)
-CI1
-#calculate z-star value for 99% confidence interval
-zstar2=qnorm(0.995)
-zstar2
-#calculate the 99& confidence interval
-CI2=c(mx-zstar2*se,mx+zstar2*se)
-CI2
-#Setting up hypothesis test
-n=40
-se
-mx
-mu=0
-mu
-#calculate the z score 
-z=(mx-mu)/se
-z
-#calculate the p-value
-pValue=2*pnorm(z)
-pValue
+# Using the functions
+mean_val = calculate_mean(mydata)
+sd_val = calculate_sd(mydata)
+se_val = calculate_se(sd_val, n)
 
+# Confidence Intervals
+ci_95 = calculate_ci(mean_val, se_val, 0.95)
+ci_99 = calculate_ci(mean_val, se_val, 0.99)
 
+# Hypothesis Testing
+h_test = hypothesis_test(mean_val, mu = 0, se_val)
+
+# Plotting
+plot_histogram_qq(mydata)
+
+# Print results
+print(ci_95)
+print(ci_99)
+print(h_test)
 
   
